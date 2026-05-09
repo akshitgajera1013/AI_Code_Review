ea1db95ce452cc00ee19b53eb07d6427c48f47d0
akshitgajera1013 <akshitgajera22@gmail.com>
2026-05-09 12:09:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+def sum(a:int,b:int) -> int:
+    return a+b
+
+
+def sub(a:int,b:int)-> int:
+    return a-b
+
---
+++ scripts/review.py
@@ -0,0 +1,42 @@
+import subprocess
+from google import genai
+import os
+import smtplib
+from email.message import EmailMessage
+
+
+def getDiff():
+    diff=subprocess.check_output(['git','show'],text=True)
+    return diff
+
+client=genai.client(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def send_email(html_content):
+    msg=EmailMessage()
+    msg['Subject']='Code Review Feedback'
+    msg['From']='akshitgajera333@gmail.com'
+    msg['To']='akshitgajera481@gmail.com'
+    msg.set_content('Please find the code review feedback below.')
+    msg.add_alternative(html_content,subtype='html') 
+
+
+    with smtplib.SMTP('smtp.gmail.com',465) as smtp:
+        smtp.login('akshitgajera333@gmail.com',os.getenv('MAIL_APP_PASSWORD'))
+        smtp.send_message(msg)
+
+    return 'Email sent successfully!'
+
+
+def main():
+    diff=getDiff()
+    prompt=f"Review the following code changes and provide feedback:\n\n Mandatory:provide the output in HTML that can use to send in mail.\n\n{diff}"
+    res=client.models.generate_content(
+        model='gemini-3-flash-preview',
+        contents=prompt
+    )
+    print('Code Review Feedback')
+    html=res.text
+    send_email(html)
+
+
+main()
